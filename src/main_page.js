5c39b2d19716393c856a4009bee1d28d61bf2e93
Harrison Bishop <harrisonbishsp@gmail.com>
2023-01-04 11:48:32 +0000

    added main section under header with burger image and text

--- src/main_page.js
+++ src/main_page.js
@@ -65,4 +65,69 @@ const mainPage = function mainPage() {
     navigation.appendChild(companyP);
     navigation.appendChild(jobsP);
   })();
+
+  (function createMainSection() {
+    const mainSection = document.createElement("main");
+    mainSection.style.width = "100%";
+    mainSection.style.height = "100%";
+    mainSection.style.backgroundColor = "rgb(240, 240, 240)";
+    allContent.appendChild(mainSection);
+
+    const mainSectionContainer = document.createElement("div");
+    mainSectionContainer.className = "main-container";
+    mainSectionContainer.style.maxWidth = "1500px";
+    mainSectionContainer.style.margin = "auto";
+    mainSectionContainer.style.display = "grid";
+    mainSectionContainer.style.gridTemplateColumns = "repeat(2, 1fr)";
+    mainSectionContainer.style.gridTemplateRows =
+      "300px 100px 450px 450px 450px";
+    mainSection.appendChild(mainSectionContainer);
+
+    const mainContainerImage = document.createElement("div");
+    mainContainerImage.style.display = "flex";
+    mainContainerImage.style.justifyContent = "center";
+    mainContainerImage.style.width = "100%";
+    mainContainerImage.style.backgroundColor = "white";
+    mainSectionContainer.appendChild(mainContainerImage);
+
+    const burgerImage = document.createElement("img");
+    burgerImage.src = "/images/burger.png";
+    burgerImage.alt = "A Beautiful Burger";
+    burgerImage.style.height = "300px";
+    burgerImage.style.width = "auto";
+    burgerImage.style.paddingTop = "10px";
+    burgerImage.style.justifySelf = "center";
+    mainContainerImage.appendChild(burgerImage);
+
+    const mainContainerText = document.createElement("div");
+    mainContainerText.style.display = "flex";
+    mainContainerText.style.justifyContent = "center";
+    mainContainerText.style.flexDirection = "column";
+    mainContainerText.style.width = "100%";
+    mainContainerText.style.backgroundColor = "white";
+    mainContainerText.style.gap = "10px";
+    mainContainerText.style.paddingLeft = "13%";
+    mainSectionContainer.appendChild(mainContainerText);
+
+    const bigText = document.createElement("p");
+    bigText.innerText = "It's time for us to show you the perfect burger.";
+    bigText.style.color = "red";
+    bigText.style.fontWeight = "bold";
+    bigText.style.fontSize = "50px";
+    bigText.style.width = "70%";
+    bigText.style.fontFamily = "'Asap', sans-serif";
+    mainContainerText.appendChild(bigText);
+
+    const smallText = document.createElement("p");
+    smallText.innerText =
+      "It's official, we've arrived...with hot, fresh, juicy burgers that are served FRESH off the grill, NOT the warming tray...";
+    smallText.style.width = "60%";
+    smallText.style.fontSize = "16px";
+    smallText.style.fontFamily = "'Roboto', sans-serif";
+    mainContainerText.appendChild(smallText);
+  })();
+
+  (function createFoodList() {
+    const mainSectionContainer = document.querySelector(".main-container");
+  })();
 };
