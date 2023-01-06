c426e50658c0274e1a7425bda0fba506b21c0198
Harrison Bishop <harrisonbishsp@gmail.com>
2023-01-06 10:53:32 +0000

    fixed id of veggie options

--- src/main_page.js
+++ src/main_page.js
@@ -161,7 +161,7 @@ const mainPage = function mainPage() {
     veggieImage.alt = "Veggie Burger";
     veggieImage.style.width = "80px";
     veggieImage.style.height = "80px";
-    veggieImage.id = "veggieImage";
+    veggieImage.id = "veggie-image";
     veggieContainer.appendChild(veggieImage);
 
     const veggieText = document.createElement("p");
